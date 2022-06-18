a6e1ee000a5af247040cf4a27637f536ec2c9859
nzyl <107577967+nzyl@users.noreply.github.com>
2022-06-18 10:30:53 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,11 @@ var seaImg,shipImg;
 function preload(){
   //uncomment the code to add animation to ship 
 
-  shipImg = loadAnimation("ship-1.png");
+  shipImg1 = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1");
   //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  shipImg = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
   
   seaImg = loadImage("sea.png");
 }
@@ -25,7 +25,7 @@ function setup(){
 
   
   ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg);
+  ship.addAnimation("movingShip",shipImg1);
   ship.scale =0.25;
   
 }
